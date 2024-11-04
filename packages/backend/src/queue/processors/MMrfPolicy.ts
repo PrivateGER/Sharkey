40472f3bd81d7cc1d97d9390a9e3e081e3bc2060
PrivateGER <privateger@privateger.me>
2024-11-04 16:14:59 +0100

    Rework MMRF handling

--- packages/backend/src/queue/processors/MMrfPolicy.ts
+++ packages/backend/src/queue/processors/MMrfPolicy.ts
@@ -1,6 +1,5 @@
-import {IActivity, IObject} from '@/core/activitypub/type.js';
+import { IActivity, IObject } from '@/core/activitypub/type.js';
 import Logger from '@/logger.js';
-
 import { IdService } from '@/core/IdService.js';
 import { ApDbResolverService } from '@/core/activitypub/ApDbResolverService.js';
 import { KeywordFilterPolicy } from '@/queue/processors/MMrfPolicies/KeywordFilterPolicy.js';
@@ -18,52 +17,40 @@ export type MMrfResponse = {
 	data: IActivity;
 }
 
-export async function runMMrf(activity: IActivity, logger: Logger, idService: IdService, apDbResolverService: ApDbResolverService): Promise<MMrfResponse> {
-	const keywordFilterPolicy = new KeywordFilterPolicy(logger);
-	const newUserSpamPolicy = new NewUserSpamPolicy(apDbResolverService, idService, logger);
-	const hellthreadPolicy = new HellthreadPolicy(logger);
-
-	if (activity.type !== 'Create') {
-		return {
-			action: MMrfAction.Neutral,
-			data: activity,
-		};
+async function applyPolicy(policy: any, activity: IActivity): Promise<MMrfResponse> {
+	let response = await policy.runPolicy(activity);
+	while (response.action === MMrfAction.RewriteNote) {
+		activity = response.data;
+		response = await policy.runPolicy(activity);
 	}
-	const object: IObject = activity.object as IObject;
-
-	if (object.type !== 'Note') {
-		return {
-			action: MMrfAction.Neutral,
-			data: activity,
-		};
-	}
-
-	let mmrfActivity = activity;
-	const keywordFilterPolicyResponse = await keywordFilterPolicy.runPolicy(mmrfActivity);
-	if (keywordFilterPolicyResponse.action === MMrfAction.RejectNote) {
-		return keywordFilterPolicyResponse;
-	} else if (keywordFilterPolicyResponse.action === MMrfAction.RewriteNote) {
-		mmrfActivity = keywordFilterPolicyResponse.data;
-	}
-
-	const newUserSpamPolicyResponse = await newUserSpamPolicy.runPolicy(mmrfActivity);
-	if (newUserSpamPolicyResponse.action === MMrfAction.RejectNote) {
-		return newUserSpamPolicyResponse;
-	} else if (newUserSpamPolicyResponse.action === MMrfAction.RewriteNote) {
-		mmrfActivity = newUserSpamPolicyResponse.data;
-	}
-
-	const hellthreadPolicyResponse = await hellthreadPolicy.runPolicy(mmrfActivity);
-	if (hellthreadPolicyResponse.action === MMrfAction.RewriteNote) {
-		mmrfActivity = hellthreadPolicyResponse.data;
-	}
-
-	return {
-		action: MMrfAction.Neutral,
-		data: mmrfActivity,
-	};
+	return response;
 }
 
-export interface MMrfPolicy {
-	runPolicy(activity: IActivity): Promise<MMrfResponse>;
+export async function runMMrf(activity: IActivity, logger: Logger, idService: IdService, apDbResolverService: ApDbResolverService): Promise<MMrfResponse> {
+	if (activity.type !== 'Create') {
+		return { action: MMrfAction.Neutral, data: activity };
+	}
+
+	const object: IObject = activity.object as IObject;
+	if (object.type !== 'Note') {
+		return { action: MMrfAction.Neutral, data: activity };
+	}
+
+	const policies = [
+		new KeywordFilterPolicy(logger),
+		new NewUserSpamPolicy(apDbResolverService, idService, logger),
+		new HellthreadPolicy(logger)
+	];
+
+	let mmrfActivity = activity;
+
+	for (const policy of policies) {
+		const response = await applyPolicy(policy, mmrfActivity);
+		if (response.action === MMrfAction.RejectNote) {
+			return response;
+		}
+		mmrfActivity = response.data;
+	}
+
+	return { action: MMrfAction.Neutral, data: mmrfActivity };
 }
