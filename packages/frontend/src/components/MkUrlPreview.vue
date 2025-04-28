85d2045edfa6939c5582f1622be171bd9a4b4868
PrivateGER <privateger@privateger.me>
2025-04-28 12:20:23 +0200

    fix missing import (bad merge)

--- packages/frontend/src/components/MkUrlPreview.vue
+++ packages/frontend/src/components/MkUrlPreview.vue
@@ -93,6 +93,7 @@ import { defineAsyncComponent, onDeactivated, onUnmounted, ref, watch } from 'vu
 import { url as local } from '@@/js/config.js';
 import { versatileLang } from '@@/js/intl-const.js';
 import * as Misskey from 'misskey-js';
+import { maybeMakeRelative } from '@@/js/url.js';
 import type { summaly } from '@transfem-org/summaly';
 import type MkNoteSimple from '@/components/MkNoteSimple.vue';
 import type SkNoteSimple from '@/components/SkNoteSimple.vue';
@@ -103,7 +104,7 @@ import MkButton from '@/components/MkButton.vue';
 import { transformPlayerUrl } from '@/scripts/player-url-transform.js';
 import { defaultStore } from '@/store.js';
 import { misskeyApi } from '@/scripts/misskey-api.js';
-import { maybeMakeRelative } from '@@/js/url.js';
+import { warningExternalWebsite } from '@/scripts/warning-external-website.js';
 
 const XNoteSimple = defineAsyncComponent<typeof MkNoteSimple | typeof SkNoteSimple>(() =>
 	defaultStore.state.noteDesign === 'misskey'
