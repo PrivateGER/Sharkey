294d89921f7f9669b1b8a8762a92579a4fcf9e0a
PrivateGER <privateger@privateger.me>
2025-04-28 17:24:47 +0200

    Add endpoint for global antennas in admin

--- packages/backend/src/server/api/endpoint-list.ts
+++ packages/backend/src/server/api/endpoint-list.ts
@@ -419,3 +419,4 @@ export * as 'users/search-by-username-and-host' from './endpoints/users/search-b
 export * as 'users/show' from './endpoints/users/show.js';
 export * as 'users/update-memo' from './endpoints/users/update-memo.js';
 export * as 'v2/admin/emoji/list' from './endpoints/v2/admin/emoji/list.js';
+export * as 'admin/antennas/global' from './endpoints/admin/antennas/global.js';
