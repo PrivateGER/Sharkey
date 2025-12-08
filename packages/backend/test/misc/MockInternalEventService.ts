76586e70489c6e9293ed301bddf53e29f64800aa
Hazelnoot <acomputerdog@gmail.com>
2025-12-08 13:56:48 -0500

    use SkEventSource as dispatch implementation for InternalEventService

--- packages/backend/test/misc/MockInternalEventService.ts
+++ packages/backend/test/misc/MockInternalEventService.ts
@@ -3,10 +3,11 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import type { Listener, ListenerProps, InternalEventTypes, EventValue, AnyListener } from '@/global/InternalEventService.js';
 import { Inject, Injectable } from '@nestjs/common';
 import type { Redis } from 'ioredis';
 import type { Config } from '@/config.js';
+import type { InternalEventContext, InternalEventProps, InternalEventTypes } from '@/global/InternalEventService.js';
+import type { AnyListener, EventListener } from '@/misc/SkEventEmitter.js';
 import { InternalEventService } from '@/global/InternalEventService.js';
 import { bindThis } from '@/decorators.js';
 import { DI } from '@/di-symbols.js';
@@ -15,7 +16,6 @@ import { TimeService } from '@/global/TimeService.js';
 import { GodOfTimeService } from './GodOfTimeService.js';
 
 type FakeCall<K extends keyof InternalEventService> = [K, Parameters<InternalEventService[K]>];
-type FakeListener = [keyof InternalEventTypes, AnyListener, ListenerProps];
 
 /**
  * Minimal implementation of InternalEventService meant for use in unit tests.
@@ -30,25 +30,19 @@ export class MockInternalEventService extends InternalEventService {
 	public _calls: FakeCall<keyof InternalEventService>[] = [];
 
 	/**
-	 * List of currently registered listeners.
-	 */
-	public _listeners: FakeListener[] = [];
-
-	/**
 	 * Resets the mock.
 	 * Clears all listeners and tracked calls.
 	 */
 	public mockReset() {
 		this._calls = [];
-		this._listeners = [];
 	}
 
 	/**
 	 * Simulates a remote event sent from another process in the cluster via redis.
 	 */
 	@bindThis
-	public async mockEmit<K extends keyof InternalEventTypes>(type: K, value: EventValue<K>): Promise<void> {
-		await this.emit(type, value, false);
+	public async mockEmitFromRedis<K extends keyof InternalEventTypes>(type: K, value: InternalEventTypes[K]): Promise<void> {
+		await super.emitLocally(type, value, { isLocal: false });
 	}
 
 	constructor(
@@ -65,40 +59,50 @@ export class MockInternalEventService extends InternalEventService {
 	}
 
 	@bindThis
-	public on<K extends keyof InternalEventTypes>(type: K, listener: Listener<K>, props?: ListenerProps): void {
-		if (!this._listeners.some(l => l[0] === type && l[1] === listener)) {
-			this._listeners.push([type, listener as AnyListener, props ?? {}]);
-		}
+	public override on<K extends keyof InternalEventTypes>(type: K, listener: EventListener<InternalEventTypes, K>, props?: Partial<InternalEventProps>): void {
 		this._calls.push(['on', [type, listener as AnyListener, props]]);
+		super.on(type, listener, props);
 	}
 
 	@bindThis
-	public off<K extends keyof InternalEventTypes>(type: K, listener: Listener<K>): void {
-		this._listeners = this._listeners.filter(l => l[0] !== type || l[1] !== listener);
+	public override off<K extends keyof InternalEventTypes>(type: K, listener: EventListener<InternalEventTypes, K>): void {
 		this._calls.push(['off', [type, listener]]);
+		super.off(type, listener);
 	}
 
 	@bindThis
-	public async emit<K extends keyof InternalEventTypes>(type: K, value: EventValue<K>, isLocal = true): Promise<void> {
-		for (const listener of this._listeners) {
-			if (listener[0] === type) {
-				if ((isLocal && !listener[2].ignoreLocal) || (!isLocal && !listener[2].ignoreRemote)) {
-					await listener[1](value, type, isLocal);
-				}
-			}
-		}
-		this._calls.push(['emit', [type, value]]);
+	public override async emit<K extends keyof InternalEventTypes>(type: K, value: InternalEventTypes[K], context?: InternalEventContext): Promise<void> {
+		this._calls.push(['emit', [type, value, context]]);
+		await super.emit(type, value, context);
 	}
 
 	@bindThis
-	public dispose(): void {
-		this._listeners = [];
+	protected override async emitExternally(): Promise<void> {
+		// Disable external emit for testing
+	}
+
+	@bindThis
+	public override connect(): void {
+		this._calls.push(['connect', []]);
+		// Disable external events for testing
+	}
+
+	@bindThis
+	public override disconnect(): void {
+		this._calls.push(['disconnect', []]);
+		// Disable external events for testing
+	}
+
+	@bindThis
+	public override dispose(): void {
 		this._calls.push(['dispose', []]);
+		super.dispose();
 	}
 
 	@bindThis
-	public onApplicationShutdown(): void {
+	public override onApplicationShutdown(): void {
 		this._calls.push(['onApplicationShutdown', []]);
+		super.onApplicationShutdown();
 	}
 
 	static create(opts?: {
