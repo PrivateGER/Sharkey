a27b64ccc94947c0a2799883aad55315bb845c23
Hazelnoot <acomputerdog@gmail.com>
2025-12-29 21:14:17 -0500

    add locales "build" script

---
+++ locales/build.js
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: hazelnoot and other Sharkey contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { build } from './index.js';
+
+console.log('Building locales...');
+build();
