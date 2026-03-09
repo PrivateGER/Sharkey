27f19426b5bf6180a91521fdc2614a03f6fe5c50
PrivateGER <privateger@privateger.me>
2026-03-09 01:13:32 +0100

    self-patch critical vuln

--- packages/backend/src/server/ActivityPubServerService.ts
+++ packages/backend/src/server/ActivityPubServerService.ts
@@ -303,6 +303,7 @@ export class ActivityPubServerService {
 		if (signature.params.headers.indexOf('digest') === -1) {
 			// Digest not found.
 			reply.code(401);
+			return;
 		} else {
 			const digest = request.headers.digest;
 
