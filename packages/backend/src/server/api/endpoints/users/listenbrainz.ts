d4896ca580ef2c8d8f88880a5411d42b2d39280e
piuvas <mail@piuvas.net>
2026-04-04 12:49:25 -0300

    some fixes to /users/listenbrainz.

--- packages/backend/src/server/api/endpoints/users/listenbrainz.ts
+++ packages/backend/src/server/api/endpoints/users/listenbrainz.ts
@@ -77,6 +77,7 @@ export const paramDef = {
 @Injectable()
 export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-disable-line import/no-default-export
 	private readonly listenBrainzCache: ManagedRedisKVCache<CachedListenBrainzEntity>;
+	private readonly listenBrainzMetadataCache: ManagedRedisKVCache<CachedListenBrainzEntity>;
 
 	constructor(
 		@Inject(DI.meta)
@@ -102,45 +103,46 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 			}
 
 			const headers: Record<string, string> = {
-				'Content-Type': 'application/json',
+				'Accept': 'application/json',
 			};
 			if (this.serverSettings.listenbrainzAuthKey) {
 				headers['Authorization'] = `Token ${this.serverSettings.listenbrainzAuthKey}`;
 			}
 
-			const res = await this.httpRequestService.send(`https://api.listenbrainz.org/1/user/${listenbrainzUsername}/playing-now`, {
-				method: 'GET',
+			const json = await this.httpRequestService.getJson<
+				{
+					payload?: {
+						listens?: Array<{
+							track_metadata?: {
+								artist_name?: string,
+								track_name?: string,
+								release_name?: string,
+								additional_info?: {
+									release_mbid?: string,
+									recording_mbid?: string,
+								},
+							},
+						}>
+					}
+				}
+			>(`https://api.listenbrainz.org/1/user/${encodeURIComponent(listenbrainzUsername)}/playing-now`,
+				undefined,
 				headers,
-				timeout: 10000,
-			}).catch((err) => {
+				undefined,
+				10000,
+			).catch((err) => {
 				this.loggerService.logger.error(`/playing-now error: ${renderInlineError(err)}`);
 				throw new ApiError(meta.errors.listenbrainzError);
 			});
 
 			// /1/user/{user}/playing-now returns an array, but it only has a single item even if you are listening from multiple
-			// devices. this happens because the return type is shared between all listenbrainz fetches.
+			// devices. this happens because the schema is shared between /listens, which returns multiple tracks.
 
-			const json = (await res.json()) as {
-				payload: {
-					listens: Array<{
-						track_metadata: {
-							artist_name: string,
-							track_name: string,
-							release_name: string,
-							additional_info: {
-								release_mbid: string | undefined,
-								recording_mbid: string | undefined,
-							},
-						},
-					}>
-				}
-			};
-
-			if (json.payload.listens.length === 0) {
+			const playingNow = json.payload?.listens?.[0];
+			if (!json.payload?.listens?.length || !playingNow?.track_metadata?.track_name || !playingNow.track_metadata.artist_name) {
 				await this.setCachedListenBrainz(listenbrainzUsername, undefined);
 				return undefined;
 			}
-			const playingNow = json.payload.listens[0];
 
 			const response: ListenBrainzResponse = {
 				title: playingNow.track_metadata.track_name,
@@ -152,39 +154,52 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 
 			// also, additional_info MAY contain release_mbid and/or recording_mbid, which are more accurate than the lookup data.
 
-			if (playingNow.track_metadata.additional_info.release_mbid) {
+			if (playingNow.track_metadata.additional_info?.release_mbid) {
 				response.coverArt =
-					`https://coverartarchive.org/release/${playingNow.track_metadata.additional_info.release_mbid}/front-250`;
+					`https://coverartarchive.org/release/${encodeURIComponent(playingNow.track_metadata.additional_info.release_mbid)}/front-250`;
 			}
 
-			if (playingNow.track_metadata.additional_info.recording_mbid) {
+			if (playingNow.track_metadata.additional_info?.recording_mbid) {
 				response.musicbrainzUrl =
-					`https://musicbrainz.org/recording/${playingNow.track_metadata.additional_info.recording_mbid}`;
+					`https://musicbrainz.org/recording/${encodeURIComponent(playingNow.track_metadata.additional_info.recording_mbid)}`;
 				response.listenbrainzUrl =
-					`https://listenbrainz.org/player?recording_mbids=${playingNow.track_metadata.additional_info.recording_mbid}`;
+					`https://listenbrainz.org/player?recording_mbids=${encodeURIComponent(playingNow.track_metadata.additional_info.recording_mbid)}`;
 			}
 
 			if ((!response.coverArt || !response.musicbrainzUrl || !response.listenbrainzUrl) && this.serverSettings.listenbrainzAuthKey) {
-				const res = await this.httpRequestService.send(
-					`https://api.listenbrainz.org/1/metadata/lookup/?artist_name=${playingNow.track_metadata.artist_name}&recording_name=${playingNow.track_metadata.track_name}`,
+				const cachedResponse = await this.getCachedMetadata(response.artist, response.title);
+				if (cachedResponse !== null) {
+					return cachedResponse;
+				}
+
+				const json = await this.httpRequestService.getJson<
 					{
-						method: 'GET',
-						headers,
-						timeout: 10000,
-					},
+						release_mbid?: string,
+						recording_mbid?: string,
+					}
+				>(
+					`https://api.listenbrainz.org/1/metadata/lookup/?artist_name=${playingNow.track_metadata.artist_name}&recording_name=${playingNow.track_metadata.track_name}`,
+					undefined,
+					headers,
+					undefined,
+					10000,
 				).catch((err) => {
-					this.loggerService.logger.error(`/metadata/lookup error: ${renderInlineError(err)}`);
+					this.loggerService.logger.error(`listenbrainz /metadata/lookup error: ${renderInlineError(err)}`);
 					throw new ApiError(meta.errors.listenbrainzError);
 				});
 
-				const json = (await res.json()) as {
-					release_mbid: string | undefined,
-					recording_mbid: string | undefined,
-				};
+				if (!json.release_mbid || !json.recording_mbid) {
+					this.loggerService.logger.warn(`listenbrainz /metadata/lookup: malformed json\n${JSON.stringify(json)}`);
+				}
+				if (json.release_mbid) {
+					response.coverArt ??= `https://coverartarchive.org/release/${encodeURIComponent(json.release_mbid)}/front-250`;
+				}
+				if (json.recording_mbid) {
+					response.listenbrainzUrl ??= `https://listenbrainz.org/player?recording_mbids=${encodeURIComponent(json.recording_mbid)}`;
+					response.musicbrainzUrl ??= `https://musicbrainz.org/recording/${encodeURIComponent(json.recording_mbid)}`;
+				}
 
-				response.coverArt ??= `https://coverartarchive.org/release/${json.release_mbid}/front-250`;
-				response.listenbrainzUrl ??= `https://listenbrainz.org/player?recording_mbids=${json.recording_mbid}`;
-				response.musicbrainzUrl ??= `https://musicbrainz.org/recording/${json.recording_mbid}`;
+				await this.setCachedMetadata(response.artist, response.title, response);
 			}
 
 			await this.setCachedListenBrainz(listenbrainzUsername, response);
@@ -195,6 +210,11 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 		this.listenBrainzCache = cacheManagementService.createRedisKVCache<CachedListenBrainzEntity>('listenbrainz', {
 			lifetime: 1000 * 5, // 5 seconds
 			memoryCacheLifetime: 1000 * 5, // 5 seconds
+		});
+
+		this.listenBrainzMetadataCache = cacheManagementService.createRedisKVCache<CachedListenBrainzEntity>('listenbrainzMetadata', {
+			lifetime: 1000 * 60 * 60 * 24, // 1 week
+			memoryCacheLifetime: 1000 * 60 * 10, // 10 minutes
 		});
 	}
 
@@ -222,11 +242,53 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 	}
 
 	@bindThis
+	private async getCachedMetadata(artist: string, track: string): Promise<ListenBrainzResponse | undefined | null> {
+		const cacheKey = `${artist}:${track}`;
+
+		const cached = await this.listenBrainzMetadataCache.get(cacheKey);
+		if (cached) {
+			if (cached.d) {
+				return {
+					title: cached.d.t,
+					artist: cached.d.a,
+					coverArt: cached.d.c,
+					listenbrainzUrl: cached.d.l,
+					musicbrainzUrl: cached.d.m,
+				};
+			} else {
+				return undefined;
+			}
+		}
+
+		// no cache entry : (
+		return null;
+	}
+
+	@bindThis
 	private async setCachedListenBrainz(username: string, data: ListenBrainzResponse | undefined): Promise<void> {
 		const cacheKey = username;
 
 		if (data) {
 			await this.listenBrainzCache.set(cacheKey, {
+				d: {
+					t: data.title,
+					a: data.artist,
+					c: data.coverArt,
+					l: data.listenbrainzUrl,
+					m: data.musicbrainzUrl,
+				},
+			});
+		} else {
+			await this.listenBrainzCache.set(cacheKey, { d: undefined });
+		}
+	}
+
+	@bindThis
+	private async setCachedMetadata(artist: string, track: string, data: ListenBrainzResponse | undefined): Promise<void> {
+		const cacheKey = `${artist}:${track}`;
+
+		if (data) {
+			await this.listenBrainzMetadataCache.set(cacheKey, {
 				d: {
 					t: data.title,
 					a: data.artist,
