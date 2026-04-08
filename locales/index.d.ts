9928151ce1a216b5a18424d2f45a9b598872db40
piuvas <mail@piuvas.net>
2026-04-08 13:33:56 -0300

    localize and improve listenbrainz user token on settings.

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -13719,6 +13719,18 @@ export interface Locale extends ILocale {
      * New root user
      */
     "newRootUser": string;
+	/*
+     * ListenBrainz Integration
+     */
+    "listenbrainzIntegration": string;
+    /**
+     * ListenBrainz User Token
+     */
+    "listenbrainzUserToken": string;
+    /**
+     * Having an user token allows Sharkey to query track metadata directly, improving reliability of cover art and links.
+     */
+    "listenbrainzUserTokenDescription": string;
 }
 declare const locales: {
     [lang: string]: Locale;
