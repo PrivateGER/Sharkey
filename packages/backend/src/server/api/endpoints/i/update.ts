f7dd9788ceae801d64f5bed5aaf9a84af8218a90
Hazelnoot <acomputerdog@gmail.com>
2026-07-02 15:38:28 -0400

    emit updateUserProfile from i/update

--- packages/backend/src/server/api/endpoints/i/update.ts
+++ packages/backend/src/server/api/endpoints/i/update.ts
@@ -622,6 +622,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 			];
 			profileUpdates.verifiedLinks = await verifyFieldLinks(newFields, profileUrls, this.httpRequestService);
 			await this.userProfilesRepository.update(user.id, profileUpdates);
+			await this.internalEventService.emit('updateUserProfile', { userId: user.id, keys: Object.keys(profileUpdates) as (keyof MiUserProfile)[] });
 
 			// Internal event purges the cache, which we immediately refill
 			await this.internalEventService.emit('userUpdated', { id: user.id });
