e31580c8fb2aec0c402dc8c528071469c7718508
mint <mint.git@thei.rs>
2026-09-02 14:30:17 +0200

    test: add more cases for verify-field-link unit tests

--- packages/backend/test/unit/misc/verify-field-link.ts
+++ packages/backend/test/unit/misc/verify-field-link.ts
@@ -16,7 +16,8 @@ describe(verifyFieldLinks, () => {
 	let httpRequestService: jest.Mocked<HttpRequestService>;
 	const testUrl = 'https://validation.test';
 	const testFields = [{ name: 'test url', value: testUrl }];
-	const testUser = 'https://sharkey.test/@user';
+	const validProfileUrl = 'https://sharkey.test/@user';
+	const invalidProfileUrl = 'https://sharkey.test/@invalid';
 
 	beforeAll(async () => {
 		app = await Test.createTestingModule({
@@ -42,26 +43,101 @@ describe(verifyFieldLinks, () => {
 		await app.close();
 	});
 
-	it('should accept a valid url in response body', async () => {
+	it('should accept a valid URL in response body', async () => {
 		httpRequestService.send.mockResolvedValue({
 			ok: true,
 			status: 200,
 			headers: new Headers(),
-			text: async () => `<link href="${testUser}" rel="me"></a>`
+			text: async () => `<link href="${validProfileUrl}" rel="me"></link>`,
 		} as Response);
-		const verifiedLinks = await verifyFieldLinks(testFields, [testUser], httpRequestService);
+		const verifiedLinks = await verifyFieldLinks(testFields, [validProfileUrl], httpRequestService);
 
 		expect(verifiedLinks).toHaveLength(1);
 		expect(verifiedLinks[0]).toEqual(testUrl);
-	})
+	});
 
-	it('should accept a valid url in headers', async () => {
+	it('should accept a valid URL in headers', async () => {
 		httpRequestService.send.mockResolvedValue({
 			ok: true,
 			status: 200,
-			headers: new Headers([['link', `<${testUser}>; rel="me"`]]),
+			headers: new Headers([['link', `<${validProfileUrl}>; rel="me"`]]),
+			text: async () => '',
 		} as Response);
-		const verifiedLinks = await verifyFieldLinks(testFields, [testUser], httpRequestService);
+		const verifiedLinks = await verifyFieldLinks(testFields, [validProfileUrl], httpRequestService);
+
+		expect(verifiedLinks).toHaveLength(1);
+		expect(verifiedLinks[0]).toEqual(testUrl);
+	});
+
+	it('should reject a user mismatch', async () => {
+		httpRequestService.send.mockResolvedValue({
+			ok: true,
+			status: 200,
+			headers: new Headers([['link', `<${invalidProfileUrl}>; rel="me"`]]),
+			text: async () => `<link href="${invalidProfileUrl}" rel="me"></link>`,
+		} as Response);
+		const verifiedLinks = await verifyFieldLinks(testFields, [validProfileUrl], httpRequestService);
+
+		expect(verifiedLinks).toHaveLength(0);
+	});
+
+	it('should only accept exact matches', async () => {
+		httpRequestService.send.mockResolvedValue({
+			ok: true,
+			status: 200,
+			headers: new Headers([['link', `<${validProfileUrl}2>; rel="me"`]]),
+			text: async () => `<link href="${validProfileUrl}2" rel="me"></link>`,
+		} as Response);
+		const verifiedLinks = await verifyFieldLinks(testFields, [validProfileUrl], httpRequestService);
+
+		expect(verifiedLinks).toHaveLength(0);
+	});
+
+	it('should reject malformatted link headers', async () => {
+		httpRequestService.send.mockResolvedValue({
+			ok: true,
+			status: 200,
+			headers: new Headers([['link', `${validProfileUrl}; rel="me"`]]),
+			text: async () => '',
+		} as Response);
+		const verifiedLinks = await verifyFieldLinks(testFields, [validProfileUrl], httpRequestService);
+
+		expect(verifiedLinks).toHaveLength(0);
+	});
+
+	it('should accept the right out of multiple link headers', async () => {
+		httpRequestService.send.mockResolvedValue({
+			ok: true,
+			status: 200,
+			headers: new Headers([['link', `<${invalidProfileUrl}>; rel="me", <${validProfileUrl}>; rel="me"`]]),
+			text: async () => '',
+		} as Response);
+		const verifiedLinks = await verifyFieldLinks(testFields, [validProfileUrl], httpRequestService);
+
+		expect(verifiedLinks).toHaveLength(1);
+		expect(verifiedLinks[0]).toEqual(testUrl);
+	});
+
+	it('should reject incorrect values for the rel attribute', async () => {
+		httpRequestService.send.mockResolvedValue({
+			ok: true,
+			status: 200,
+			headers: new Headers([['link', `<${validProfileUrl}>; rel="invalid"`]]),
+			text: async () => `<link href="${validProfileUrl}" rel="invalid"></link>`,
+		} as Response);
+		const verifiedLinks = await verifyFieldLinks(testFields, [validProfileUrl], httpRequestService);
+
+		expect(verifiedLinks).toHaveLength(0);
+	});
+
+	it('should not return duplicate matches', async () => {
+		httpRequestService.send.mockResolvedValue({
+			ok: true,
+			status: 200,
+			headers: new Headers([['link', `<${validProfileUrl}>; rel="me", <${validProfileUrl}>; rel="me"`]]),
+			text: async () => `<link href="${validProfileUrl}" rel="me"></link><a href="${validProfileUrl}" rel="me"></a>`,
+		} as Response);
+		const verifiedLinks = await verifyFieldLinks(testFields, [validProfileUrl], httpRequestService);
 
 		expect(verifiedLinks).toHaveLength(1);
 		expect(verifiedLinks[0]).toEqual(testUrl);
